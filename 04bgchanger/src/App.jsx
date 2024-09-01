ea0b872be6b2de17c532d59867ce6404533ad956
Saif Ali <86141025+Ccybertron@users.noreply.github.com>
2024-09-01 07:29:04 +0500

    Add files via upload

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+   <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+
+    <div className="fixed flex flex-wrap bottom-12 justify-center inset-x-0 mt-8  px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-xl">
+        <button
+        onClick={()=> setColor("red")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"red"}}
+        >Red</button>
+        <button
+        onClick={()=> setColor("green")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"green"}}
+        >Green</button>
+        <button
+
+        onClick={()=> setColor("blue")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"Blue"}}
+        >Blue</button>
+        </div>
+    </div>
+   </div>
+  )
+}
+
+export default App
