8ae316c973b4e9de6aaea8bdaa024da923e4ee30
Saif Ali <86141025+Ccybertron@users.noreply.github.com>
2024-09-13 14:27:01 +0500

    Add files via upload

---
+++ 11reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/Addtodo'
+import Todos from './components/Todos'
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Learn about Redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+       
+    </>
+  )
+}
+
+export default App
